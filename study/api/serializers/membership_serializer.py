8364b9c0f5a3548cb3e0fcace31ede6d7296c0c7
Daniel Santos <drsantos20@gmail.com>
2021-03-11 01:10:46 -0300

    Add membership serializer

--- study/api/serializers/membership_serializer.py
+++ study/api/serializers/membership_serializer.py
@@ -1,21 +1,35 @@
 from rest_framework import serializers
 
-from study.api.models.study_plan import StudyPlan
+from study.api.models import Subscription, UserMembership, Membership
 
 
 class MembershipSerializer(serializers.ModelSerializer):
     class Meta:
-        model = StudyPlan
+        model = Membership
         fields = ['slug', 'membership_type', 'price']
 
 
 class UserMembershipSerializer(serializers.ModelSerializer):
+    membership = MembershipSerializer()
+
     class Meta:
-        model = StudyPlan
+        model = UserMembership
         fields = ['user', 'membership']
+
+    def create(self, validated_data):
+        membership_data = validated_data.pop('membership')
+        user_membership = UserMembership.objects.create(**validated_data)
+        Membership.objects.create(user_membership=user_membership, **membership_data)
+        return user_membership
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
     class Meta:
-        model = StudyPlan
+        model = Subscription
         fields = ['user_membership', 'active']
+
+    def create(self, validated_data):
+        user_membership_data = validated_data.pop('user_membership')
+        subscription = Subscription.objects.create(**validated_data)
+        UserMembership.objects.create(subscription=subscription, **user_membership_data)
+        return subscription
